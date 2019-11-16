77153d1050354911850dd1e82dece1e12c4ce74a
alx <37true@gmail.com>
2019-11-16 19:02:25 +0700

    init

---
+++ src/main/java/tk/alltrue/spring1/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package tk.alltrue.spring1;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ src/main/java/tk/alltrue/spring1/Music.java
@@ -0,0 +1,5 @@
+package tk.alltrue.spring1;
+
+public interface Music {
+    String getSong();
+}
