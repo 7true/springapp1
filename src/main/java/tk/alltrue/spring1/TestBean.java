6fc643ef049dcd2993b7d07aeac0224024386563
alx <37true@gmail.com>
2019-11-16 14:52:13 +0700

    Initial commit

---
+++ src/main/java/tk/alltrue/spring1/TestBean.java
@@ -0,0 +1,18 @@
+package tk.alltrue.spring1;
+
+public class TestBean {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+}
